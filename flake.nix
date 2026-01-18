1d95d4425f810c3240b7a02d62217eb7b93bdb8e
rencire <546296+rencire@users.noreply.github.com>
2026-01-17 17:06:58 -0800

    fix: check if attribute exists before wrapping it

--- flake.nix
+++ flake.nix
@@ -3,42 +3,80 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    flakelight.url = "github:nix-community/flakelight";
   };
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      nix-wrapper-modules,
-    }:
-    {
-      lib = {
-        configure =
-          {
-            pkgs,
-            configDir,
-            packages,
-            extraArgs ? { },
-          }:
-          let
-            lib = nixpkgs.lib;
-            moduleArgs = {
-              inherit pkgs lib;
-            }
-            // extraArgs;
-          in
-          map (
-            pkg:
+    { flakelight, ... }@inputs:
+    flakelight ./. (
+      { lib, ... }:
+      {
+        inherit inputs;
+        systems = lib.mkForce [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ];
+        # Your library functions
+        lib = {
+          configure =
+            {
+              pkgs,
+              configDir,
+              packages,
+              extraArgs ? { },
+            }:
             let
-              pkgName = pkg.pname or pkg.name;
-              configPath = configDir + "/${pkgName}.nix";
-              customConfig = if builtins.pathExists configPath then import configPath moduleArgs else { };
-              wrapArgs = {
-                inherit pkgs;
+              lib = inputs.nixpkgs.lib;
+              moduleArgs = {
+                inherit pkgs lib;
               }
-              // customConfig;
+              // extraArgs;
             in
-            nix-wrapper-modules.wrappedModules."${pkgName}".wrap wrapArgs
-          ) packages;
-      };
-    };
+            map (
+              pkg:
+              let
+                pkgName = pkg.pname or pkg.name;
+                configPath = configDir + "/${pkgName}.nix";
+                customConfig = if builtins.pathExists configPath then import configPath moduleArgs else { };
+
+                # Check if wrapper exists
+                hasWrapper = inputs.nix-wrapper-modules.wrappedModules ? "${pkgName}";
+
+                wrapArgs = {
+                  inherit pkgs;
+                }
+                // customConfig;
+              in
+              if hasWrapper then inputs.nix-wrapper-modules.wrappedModules."${pkgName}".wrap wrapArgs else pkg
+            ) packages;
+        };
+        # Checks
+        checks = pkgs: {
+          test-configure-invalid-package =
+            let
+              fakePackage = pkgs.runCommand "fake" { } "touch $out" // {
+                pname = "nonexistent-wrapper-package";
+              };
+
+              evalResult = builtins.tryEval (
+                let
+                  packages = inputs.self.lib.configure {
+                    inherit pkgs;
+                    configDir = pkgs.emptyDirectory;
+                    packages = [ fakePackage ];
+                  };
+                in
+                # Force evaluation of the fakePackage in order to validate that we
+                # are calling inputs.nix-wrapper-modules correctly
+                # If not, then this test will error before reaching our assertion
+                builtins.seq (builtins.elemAt packages 0) true
+              );
+            in
+            pkgs.writeText "test-configure-invalid-package" (
+              assert evalResult.success;
+              "success"
+            );
+        };
+      }
+    );
 }
